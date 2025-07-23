baf818d101d14920a6d0c7deeb97e5906e7cb0fb
dev-ankit24 <ankitkumar839282@gmail.com>
2025-07-23 13:09:09 +0530

    changes

--- src/componets/ComingSoon.jsx
+++ src/componets/ComingSoon.jsx
@@ -72,7 +72,7 @@ let [show,setShow]=useState(false)
     <div className="min-h-screen mt-[80px] bg-gray-50 md:bg-[#FFBFOO] flex flex-col justify-center items-center text-center px-4">
       {/* Header Text */}
       <h1 className="text-4xl sm:text-5xl font-bold text-gray-800 mb-4">
-         Curious About Launch Perks? 
+         Curious About Launch Perks.
         
         
       </h1>
